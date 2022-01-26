980d21be9fb36c0d69bbc26d577d1d176d8d8cfc
ganbitto82 <andresthekig@gmail.com>
2022-01-26 01:25:54 -0300

    problema de compatibilidad solucionado

--- app/src/main/java/com/example/compraventa/MainActivity.java
+++ app/src/main/java/com/example/compraventa/MainActivity.java
@@ -2,26 +2,21 @@ package com.example.compraventa;
 
 import static java.lang.Integer.parseInt;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.SeekBar;
-import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import Model.Categoria;
 
 public class MainActivity extends AppCompatActivity {
    // private Spinner spinner;
